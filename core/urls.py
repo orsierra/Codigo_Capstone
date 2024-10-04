1f2cc5395c8b01df99027b607d69c2d3cd5e3202
orsierra <or.sierra@duocuc.cl>
2024-10-04 18:30:36 -0300

    URLS y VIEWS

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,25 @@ from . import views
 urlpatterns = [
     path('', views.home, name='login'),
     path('login/', views.login_view, name='login'),
+    #DIRECTOR
     path('director/', views.director_view, name='director'),
+    path('directorFinanzas/',views.directorFinanzas_view, name='directorFinanzas'),
+    path('directorInforme/',views.directorInforme_view, name='directorInforme'),
+    path('directorMenu/',views.directorMenu_view, name='directorMenu'),
+    path('directorPlanificacion/',views.directorPlanificacion_view, name='directorPlanificacion'),    
+    #PROFESOR
+    path('profesorAsistencia/', views.profesorAsistencia_view, name='profesorAsistencia'),
+    path('profesorCalificacion/', views.profesorCalificacion_view, name='profesorCalificacion'),
+    path('profesorMisCursos/', views.profesorMisCursos_view, name='profesorMisCursos'),
+    path('profesorObservacion/', views.profesorObservacion_view, name='profesorObservacion'),
+      
+      
     path('estudiante/', views.estudiante_view, name='estudiante'),
     path('sostenedor/', views.sostenedor_view, name='sostenedor'),
     path('estudiante_pru_base/', views.estudiante_pru_base, name='estudiante_pru_base'), 
+    
+    #APODERADO
+    path('apoderado/',views.apoderado_view, name ='apoderado'),
+    path('apoderadoAsistencia/', views.apoderadoAsistencia_view, name='apoderadoAsistencia'),
+    path('apoderadoMatricula/', views.apoderadoMatricula_view, name='apoderadoMatricula'),
 ]
--- core/views.py
+++ core/views.py
@@ -8,8 +8,25 @@ def home(request):
 def login_view(request):
     return render(request, 'login.html')
 
+#DIRECTOR
+
 def director_view(request):
     return render(request, 'director.html')
+
+def directorFinanzas_view(request):
+    return render(request, 'directorFinanzas.html')
+
+def directorInforme_view(request):
+    return render(request, 'directorInforme.html')
+
+def directorMenu_view(request):
+    return render(request, 'directorMenu.html')
+
+def directorPlanificacion_view(request):
+    return render(request, 'directorPlanificacion.html')
+
+#ESTUDIANTE
+
 
 def estudiante_view(request):
     return render(request, 'estudiante.html')
@@ -20,3 +37,27 @@ def sostenedor_view(request):
 def estudiante_pru_base(request):
     return render(request, 'estudiante_pru_base.html')
 
+#APODERADO
+
+def apoderado_view(request):
+    return render(request, 'apoderado.html')
+
+def apoderadoAsistencia_view(request):
+    return render(request, 'apoderadoAsistencia.html')
+
+def apoderadoMatricula_view(request):
+    return render(request, 'apoderadoMatricula.html')
+
+#PROFESOR
+
+def profesorAsistencia_view(request):
+    return render(request, 'profesorAsistencia.html')
+
+def profesorCalificacion_view(request):
+    return render(request, 'profesorCalificacion.html')
+
+def profesorMisCursos_view(request):
+    return render(request, 'profesorMisCursos.html')
+
+def profesorObservacion_view(request):
+    return render(request, 'profesorObservacion.html')
