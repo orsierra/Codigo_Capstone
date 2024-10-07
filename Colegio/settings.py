902e3cf9a74f2529118c36c6dd59cc01052a9e5c
orsierra <or.sierra@duocuc.cl>
2024-10-07 15:56:43 -0300

    psycopg2

--- Colegio/settings.py
+++ Colegio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Colegio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
