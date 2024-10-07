16ed59eeafee639ff522c77172a9f1571c7eb13c
patokennedy <patr.vergara@duocuc.cl>
2024-10-07 16:50:13 -0300

    a

--- Colegio/settings.py
+++ Colegio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'USER': 'admin',
         'PASSWORD': '123456',
         'HOST': 'localhost',
-        'PORT': '5433',
+        'PORT': '5432',
     }
 }
 
@@ -127,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # For production
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+#STATICFILES_DIRS = [
+  #  BASE_DIR / "static",
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
