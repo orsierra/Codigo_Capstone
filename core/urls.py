d0554c6a590dd6c24082662a38af24c22a442f79
benjaTapia <benj.tapia@duocuc.cl>
2024-10-09 18:45:36 -0300

    refresh

--- core/urls.py
+++ core/urls.py
@@ -12,12 +12,7 @@ urlpatterns = [
     path('directorMenu/',views.directorMenu_view, name='directorMenu'),
     path('directorPlanificacion/',views.directorPlanificacion_view, name='directorPlanificacion'),    
     #PROFESOR
-    path('asistencia/', profesor_asistencia_view, name='profesor_asistencia'),
-    path('asistencia/<int:curso_id>/', registrar_asistencia_view, name='registrar_asistencia'),  # Asegúrate de que esto esté correcto
 
-    path('asistencia/<int:curso_id>/', registrar_asistencia_view, name='registrar_asistencia'),
-    path('asistencia/', profesor_asistencia_view, name='profesor_asistencia'),
-    
     path('estudiante/', views.estudiante_view, name='estudiante'),
     path('sostenedor/', views.sostenedor_view, name='sostenedor'),
     path('estudiante_pru_base/', views.estudiante_pru_base, name='estudiante_pru_base'), 
