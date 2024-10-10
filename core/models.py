124f4a6511495ef0d7cecb83f08e6c746f7d153c
orsierra <or.sierra@duocuc.cl>
2024-10-10 17:48:07 -0300

    new model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,3 @@
-# models.py
-from django.db import models
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
@@ -7,13 +5,15 @@ from django.db import models
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
 
 class Apoderado(models.Model):
     id_apoderado = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=50)
     direccion = models.CharField(max_length=70)
-    correo = models.CharField(max_length=60)
+    corrreo = models.CharField(max_length=60)
     telefono = models.CharField(max_length=15)
     relacion_con_estudiante = models.CharField(max_length=15)
     contrasena = models.CharField(max_length=20)
@@ -39,19 +39,17 @@ class Asistencia(models.Model):
     id_asistencia = models.IntegerField(primary_key=True)
     presente = models.BooleanField()
     justificativo = models.CharField(max_length=50)
-    clase_id_clase = models.ForeignKey('Clase', on_delete=models.CASCADE, db_column='clase_id_clase')
-
+    clase_id_clase = models.ForeignKey('Clase', models.DO_NOTHING, db_column='clase_id_clase')
 
     class Meta:
         managed = True
         db_table = 'asistencia'
 
 
-
 class Cargaacademica(models.Model):
     id_carga_academica = models.IntegerField(primary_key=True)
     horas_semanales = models.IntegerField()
-    profesor_id_profesor = models.OneToOneField('Profesor', models.DO_NOTHING, db_column='profesor_id_profesor')
+    profesor_id_profesor = models.OneToOneField('Profesor', models.DO_NOTHING, db_column='profesor_id_profesor', blank=True, null=True)
 
     class Meta:
         managed = True
@@ -91,7 +89,7 @@ class Curso(models.Model):
     grado = models.CharField(max_length=15)
     seccion = models.CharField(max_length=15)
     año_lectivo = models.DateField()
-    estudiante_id_estudiante = models.OneToOneField('Estudiante', models.DO_NOTHING, db_column='estudiante_id_estudiante')
+    estudiante_id_estudiante = models.OneToOneField('Estudiante', models.DO_NOTHING, db_column='estudiante_id_estudiante', blank=True, null=True)
 
     class Meta:
         managed = True
@@ -104,13 +102,13 @@ class Director(models.Model):
     correo = models.CharField(max_length=100)
     telefono = models.CharField(max_length=20)
     apellido = models.CharField(max_length=30)
-    establecimiento_id_establecimiento = models.OneToOneField('Establecimiento', models.DO_NOTHING, db_column='establecimiento_id_establecimiento')     
+    establecimiento_id_establecimiento = models.OneToOneField('Establecimiento', models.DO_NOTHING, db_column='establecimiento_id_establecimiento', blank=True, null=True)
     contrasena = models.CharField(max_length=20)
 
     class Meta:
         managed = True
         db_table = 'director'
- 
+
 
 
 class Empleado(models.Model):
@@ -120,7 +118,7 @@ class Empleado(models.Model):
     telefono = models.CharField(max_length=20)
     apellido = models.CharField(max_length=100)
     cargo = models.CharField(max_length=30)
-    establecimiento_id_establecimiento = models.ForeignKey('Establecimiento', models.DO_NOTHING, db_column='establecimiento_id_establecimiento')
+    establecimiento_id_establecimiento = models.ForeignKey('Establecimiento', models.DO_NOTHING, db_column='establecimiento_id_establecimiento', blank=True, null=True)  
     contrasena = models.CharField(max_length=20)
 
     class Meta:
@@ -134,8 +132,8 @@ class Establecimiento(models.Model):
     direccion = models.CharField(max_length=50)
     telefono = models.CharField(max_length=15)
     email = models.CharField(max_length=60)
-    estudiante_id_estudiante = models.OneToOneField('Estudiante', models.DO_NOTHING, db_column='estudiante_id_estudiante')
-    director_id_director = models.OneToOneField(Director, models.DO_NOTHING, db_column='director_id_director')
+    estudiante_id_estudiante = models.OneToOneField('Estudiante', models.DO_NOTHING, db_column='estudiante_id_estudiante', blank=True, null=True)
+    director_id_director = models.OneToOneField(Director, models.DO_NOTHING, db_column='director_id_director', blank=True, null=True)
 
     class Meta:
         managed = True
@@ -149,12 +147,12 @@ class Estudiante(models.Model):
     fecha_nacimiento = models.DateField()
     direccion = models.CharField(max_length=50)
     genero = models.CharField(max_length=15)
-    curso_id_curso = models.OneToOneField(Curso, models.DO_NOTHING, db_column='curso_id_curso')
-    examen_inicial_id_examen = models.OneToOneField('ExamenInicial', models.DO_NOTHING, db_column='examen_inicial_id_examen')
-    establecimiento_id_establecimiento = models.OneToOneField(Establecimiento, models.DO_NOTHING, db_column='establecimiento_id_establecimiento')       
+    curso_id_curso = models.OneToOneField(Curso, models.DO_NOTHING, db_column='curso_id_curso', blank=True, null=True)
+    examen_inicial_id_examen = models.OneToOneField('ExamenInicial', models.DO_NOTHING, db_column='examen_inicial_id_examen', blank=True, null=True)
+    establecimiento_id_establecimiento = models.OneToOneField(Establecimiento, models.DO_NOTHING, db_column='establecimiento_id_establecimiento', blank=True, null=True) 
     correo = models.CharField(max_length=150)
     contrasena = models.CharField(max_length=20)
-    apoderado_id_apoderado = models.ForeignKey(Apoderado, models.DO_NOTHING, db_column='apoderado_id_apoderado')
+    apoderado_id_apoderado = models.ForeignKey(Apoderado, models.DO_NOTHING, db_column='apoderado_id_apoderado', blank=True, null=True)
 
     class Meta:
         managed = True
@@ -165,7 +163,7 @@ class Evaluacion(models.Model):
     id_evaluacion = models.IntegerField(primary_key=True)
     tipo_evaluacion = models.CharField(max_length=100)
     nota = models.FloatField()
-    asignatura_id_asignatura = models.ForeignKey(Asignatura, models.DO_NOTHING, db_column='asignatura_id_asignatura')
+    asignatura_id_asignatura = models.ForeignKey(Asignatura, models.DO_NOTHING, db_column='asignatura_id_asignatura', blank=True, null=True)
 
     class Meta:
         managed = True
@@ -176,7 +174,7 @@ class ExamenInicial(models.Model):
     id_examen = models.IntegerField(primary_key=True)
     fecha_examen = models.DateField()
     nota = models.FloatField()
-    estudiante_id_estudiante = models.OneToOneField(Estudiante, models.DO_NOTHING, db_column='estudiante_id_estudiante')
+    estudiante_id_estudiante = models.OneToOneField(Estudiante, models.DO_NOTHING, db_column='estudiante_id_estudiante', blank=True, null=True)
 
     class Meta:
         managed = True
@@ -189,8 +187,8 @@ class Informeacademico(models.Model):
     fecha_emision = models.DateField()
     promedio = models.FloatField()
     observacion = models.CharField(max_length=400)
-    estudiante_id_estudiante = models.ForeignKey(Estudiante, models.DO_NOTHING, db_column='estudiante_id_estudiante')
-    periodoacademico_id_periodo_academico = models.ForeignKey('Periodoacademico', models.DO_NOTHING, db_column='periodoacademico_id_periodo_academico') 
+    estudiante_id_estudiante = models.ForeignKey(Estudiante, models.DO_NOTHING, db_column='estudiante_id_estudiante', blank=True, null=True)
+    periodoacademico_id_periodo_academico = models.ForeignKey('Periodoacademico', models.DO_NOTHING, db_column='periodoacademico_id_periodo_academico', blank=True, null=True)
 
     class Meta:
         managed = True
@@ -202,8 +200,8 @@ class Informefinanza(models.Model):
     saldo = models.IntegerField()
     fecha_emision = models.DateField()
     descripcion = models.CharField(max_length=100)
-    contrato_id_contrato = models.ForeignKey(Contrato, models.DO_NOTHING, db_column='contrato_id_contrato')
-    apoderado_id_apoderado = models.ForeignKey(Apoderado, models.DO_NOTHING, db_column='apoderado_id_apoderado')
+    contrato_id_contrato = models.ForeignKey(Contrato, models.DO_NOTHING, db_column='contrato_id_contrato', blank=True, null=True)
+    apoderado_id_apoderado = models.ForeignKey(Apoderado, models.DO_NOTHING, db_column='apoderado_id_apoderado', blank=True, null=True)
 
     class Meta:
         managed = True
@@ -216,7 +214,7 @@ class Matricula(models.Model):
     fecha_solicitud = models.DateField()
     fecha_confirmacion = models.DateField()
     estado = models.CharField(max_length=20)
-    estudiante_id_estudiante = models.ForeignKey(Estudiante, models.DO_NOTHING, db_column='estudiante_id_estudiante')
+    estudiante_id_estudiante = models.ForeignKey(Estudiante, models.DO_NOTHING, db_column='estudiante_id_estudiante', blank=True, null=True)
 
     class Meta:
         managed = True
@@ -226,7 +224,7 @@ class Matricula(models.Model):
 class Observacion(models.Model):
     id_observacion = models.IntegerField(primary_key=True)
     observacion = models.CharField(max_length=500)
-    estudiante_id_estudiante = models.ForeignKey(Estudiante, models.DO_NOTHING, db_column='estudiante_id_estudiante')
+    estudiante_id_estudiante = models.ForeignKey(Estudiante, models.DO_NOTHING, db_column='estudiante_id_estudiante', blank=True, null=True)
 
     class Meta:
         managed = True
@@ -234,7 +232,7 @@ class Observacion(models.Model):
 
 
 class Periodoacademico(models.Model):
-    id_periodo_academico = models.CharField(primary_key=True, max_length=15)
+    id_periodo_academico = models.IntegerField(primary_key=True)
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
     estado = models.CharField(max_length=15)
@@ -250,8 +248,8 @@ class Profesor(models.Model):
     apellido = models.CharField(max_length=50)
     título = models.CharField(max_length=60)
     area = models.CharField(max_length=30)
-    cargaacademica_id_carga_academica = models.OneToOneField(Cargaacademica, models.DO_NOTHING, db_column='cargaacademica_id_carga_academica')
-    establecimiento_id_establecimiento = models.ForeignKey(Establecimiento, models.DO_NOTHING, db_column='establecimiento_id_establecimiento')
+    cargaacademica_id_carga_academica = models.OneToOneField(Cargaacademica, models.DO_NOTHING, db_column='cargaacademica_id_carga_academica', blank=True, null=True)    
+    establecimiento_id_establecimiento = models.ForeignKey(Establecimiento, models.DO_NOTHING, db_column='establecimiento_id_establecimiento', blank=True, null=True)    
     correo = models.CharField(max_length=150)
     contrasena = models.CharField(max_length=20)
 
@@ -260,43 +258,43 @@ class Profesor(models.Model):
         db_table = 'profesor'
 
 
-class Relation10(models.Model):
+class RelacionCursoAsignatura(models.Model):
     curso_id_curso = models.OneToOneField(Curso, models.DO_NOTHING, db_column='curso_id_curso', primary_key=True)  # The composite primary key (curso_id_curso, asignatura_id_asignatura) found, that is not supported. The first column is selected.
     asignatura_id_asignatura = models.ForeignKey(Asignatura, models.DO_NOTHING, db_column='asignatura_id_asignatura')
 
     class Meta:
         managed = True
-        db_table = 'relation_10'
+        db_table = 'relacion_curso_asignatura'
         unique_together = (('curso_id_curso', 'asignatura_id_asignatura'),)
 
 
-class Relation28(models.Model):
+class RelacionCursoClase(models.Model):
     curso_id_curso = models.OneToOneField(Curso, models.DO_NOTHING, db_column='curso_id_curso', primary_key=True)  # The composite primary key (curso_id_curso, clase_id_clase) found, that is not supported. The first column is selected.
     clase_id_clase = models.ForeignKey(Clase, models.DO_NOTHING, db_column='clase_id_clase')
 
     class Meta:
         managed = True
-        db_table = 'relation_28'
+        db_table = 'relacion_curso_clase'
         unique_together = (('curso_id_curso', 'clase_id_clase'),)
 
 
-class Relation8(models.Model):
+class RelacionCursoProfesor(models.Model):
     curso_id_curso = models.OneToOneField(Curso, models.DO_NOTHING, db_column='curso_id_curso', primary_key=True)  # The composite primary key (curso_id_curso, profesor_id_profesor) found, that is not supported. The first column is selected.
     profesor_id_profesor = models.ForeignKey(Profesor, models.DO_NOTHING, db_column='profesor_id_profesor')
 
     class Meta:
         managed = True
-        db_table = 'relation_8'
+        db_table = 'relacion_curso_profesor'
         unique_together = (('curso_id_curso', 'profesor_id_profesor'),)
 
 
-class Relation9(models.Model):
+class RelacionCursoSala(models.Model):
     curso_id_curso = models.OneToOneField(Curso, models.DO_NOTHING, db_column='curso_id_curso', primary_key=True)  # The composite primary key (curso_id_curso, sala_id_sala) found, that is not supported. The first column is selected.
     sala_id_sala = models.ForeignKey('Sala', models.DO_NOTHING, db_column='sala_id_sala')
 
     class Meta:
         managed = True
-        db_table = 'relation_9'
+        db_table = 'relacion_curso_sala'
         unique_together = (('curso_id_curso', 'sala_id_sala'),)
 
 
@@ -304,7 +302,7 @@ class Respaldoacademico(models.Model):
     id_respaldo_academico = models.IntegerField(primary_key=True)
     fecha_respaldo = models.DateField()
     descripcion = models.CharField(max_length=30)
-    periodoacademico_id_periodo_academico = models.ForeignKey(Periodoacademico, models.DO_NOTHING, db_column='periodoacademico_id_periodo_academico')   
+    periodoacademico_id_periodo_academico = models.ForeignKey(Periodoacademico, models.DO_NOTHING, db_column='periodoacademico_id_periodo_academico')
 
     class Meta:
         managed = True
