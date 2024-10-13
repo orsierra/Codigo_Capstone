413d64c6cec79d6591123e7c42de06b526bdefd9
patokennedy <patr.vergara@duocuc.cl>
2024-10-13 17:21:11 -0300

    a

--- Colegio/settings.py
+++ Colegio/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'Colegio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'school',
-        'USER': 'admin',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
