d61260fd56ea110a74203b54f3ea93494b3939b0
patokennedy <patr.vergara@duocuc.cl>
2024-10-14 11:11:56 -0300

    asistencia profe

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,20 @@
 from django import forms
-from .models import Asistencia, Calificacion
+from .models import Asistencia, Alumno, Calificacion
 
 class AsistenciaForm(forms.ModelForm):
     class Meta:
         model = Asistencia
-        fields = ['alumno', 'curso', 'fecha', 'presente']
+        fields = ['curso', 'fecha']  # Puedes quitar el campo 'alumno' porque será manejado en la vista y el HTML.
+
+    # Agregar campos dinámicamente para los alumnos
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'alumnos' in kwargs:
+            for alumno in kwargs['alumnos']:
+                self.fields[f'presente_{alumno.id}'] = forms.BooleanField(required=False, label=f'{alumno.nombre} {alumno.apellido} presente')
+                self.fields[f'ausente_{alumno.id}'] = forms.BooleanField(required=False, label=f'{alumno.nombre} {alumno.apellido} ausente')
+                self.fields[f'justificado_{alumno.id}'] = forms.BooleanField(required=False, label=f'{alumno.nombre} {alumno.apellido} justificado')
+
 
 class CalificacionForm(forms.ModelForm):
     class Meta:
