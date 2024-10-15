97f04b5c6b224e24af33daf60af96edae9a83f0c
benjaTapia <benj.tapia@duocuc.cl>
2024-10-15 18:44:59 -0300

    refresh

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Asistencia, Alumno, Calificacion
+from .models import Asistencia, Alumno, Calificacion, Observacion
 from django.core.exceptions import ValidationError
 class AsistenciaForm(forms.ModelForm):
     class Meta:
@@ -31,3 +31,24 @@ class CalificacionForm(forms.ModelForm):
         if nota < 0 or nota > 7:
             raise ValidationError('La nota debe estar entre 0 y 7.')
         return nota
+
+
+class ObservacionForm(forms.ModelForm):
+    alumno = forms.ModelChoiceField(queryset=Alumno.objects.none(), label='Alumno')  # Inicialmente vacío
+
+    class Meta:
+        model = Observacion
+        fields = ['alumno', 'contenido', 'fecha']  # Asegúrate de incluir el campo de alumno
+        widgets = {
+            'contenido': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Escribe aquí la observación...'}),
+            'fecha': forms.DateInput(attrs={'type': 'date'}),
+        }
+        labels = {
+            'contenido': 'Contenido de la Observación',
+            'fecha': 'Fecha',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ObservacionForm, self).__init__(*args, **kwargs)
+        self.fields['contenido'].widget.attrs.update({'class': 'form-control'})
+        self.fields['fecha'].widget.attrs.update({'class': 'form-control'})
