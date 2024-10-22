9fc1fc1b10394136d02fde3a3c6a9f0112c3a3da
benjaTapia <benj.tapia@duocuc.cl>
2024-10-22 19:31:31 -0300

    refresh

--- core/urls.py
+++ core/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     #====================================================================================================
     path('informe-financiero/', views.informe_financiero_view, name='informe_financiero'),
     path('informe-financiero/editar/<int:id>/', views.editar_informe_view, name='editar_informe'),
-    path('informe-financiero/eliminar/<int:id>/', views.eliminar_informe_view, name='eliminar_informe'),
+    path('informe-financiero/eliminar/<int:informe_id>/', views.eliminar_informe_view, name='eliminar_informe'),
     path('informe-financiero/pdf/', views.generar_pdf_view, name='descargar_pdf'),
     # Asistende De admision y Matricula
     path('gestionar_estudiantes/', views.gestionar_estudiantes, name='gestionar_estudiantes'),
